47ff1777f743c5a88553122167961ddf606a431c
Miguel Lameg <arturomiguel.lameg@glasgow.ac.uk>
2026-02-20 23:06:16 +0000

    resolve merge conflict

--- Card.gd
+++ Card.gd
@@ -1,6 +1,12 @@
-extends Node
+extends Node2D
 
 class_name Card
+
+
+var offset: Vector2
+var initial_pos: Vector2
+var at_drop_area = false
+
 
 var card_values: Dictionary[Values.Maat, int] = {
 	Values.Maat.balance: 0,
@@ -17,4 +23,9 @@ func onChoose() -> void:
 	for k in card_values:
 		if card_values[k] != 0:
 			global_score.game_score[k] = card_values[k]
-	pass
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_left"):
+		initial_pos = global_position
+		offset = get_global_mouse_position() - global_position
+		World.set_dr
--- message_bus.gd
+++ message_bus.gd
@@ -3,3 +3,4 @@ extends Node
 signal ROUND_END
 signal INTRO_END
 signal ALL_ROUNDS_DONE
+signal INTRO_START
