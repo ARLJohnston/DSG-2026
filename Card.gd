8b75406d5a3176cdd560d855a528493a3bb8d999
Miguel Lameg <arturomiguel.lameg@glasgow.ac.uk>
2026-02-20 20:07:50 +0000

    init Card Scene

---
+++ Card.gd
@@ -0,0 +1,21 @@
+extends Node
+
+class_name Card
+
+var card_values = {
+	Values.Maat.balance: 0,
+ 	Values.Maat.harmony: 0,
+ 	Values.Maat.justice: 0,
+ 	Values.Maat.law: 0,
+ 	Values.Maat.morality:0,
+ 	Values.Maat.order:0,
+ 	Values.Maat.truth: 0  
+}
+
+
+func onChoose() -> void:
+	var m = Values.new()
+	for k in card_values:
+		if card_values[k] != 0:
+			global_score.game_score[k] = card_values[k]
+	pass
--- values.gd
+++ values.gd
@@ -1,9 +1,15 @@
 extends Node
 
-var truth: int
-var balance: int
-var order: int
-var harmony: int
-var law: int
-var morality: int
-var justice: int
+class_name Values
+
+enum Maat {truth, balance, order, harmony, law, morality, justice}
+
+
+
+func score(foo,bar) -> void:
+	
+	# inits
+	if foo not in global_score.game_score.keys():
+		global_score.game_score[foo] = 0
+		
+	global_score.game_score[foo] += bar
