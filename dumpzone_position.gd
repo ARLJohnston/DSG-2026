847107984f680d67d53db3ff13493ea37f2d1261
Rory <rory.mcdermid@fdmgroup.com>
2026-02-22 15:21:05 +0000

    fire moved

--- dumpzone_position.gd
+++ dumpzone_position.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 func _ready() -> void:
 	var screenDimensions: Vector2 = get_viewport().get_visible_rect().size
-	self.position = Vector2(screenDimensions.x/2, 0.9*screenDimensions.y)
+	#self.position = Vector2(screenDimensions.x/2, 0.9*screenDimensions.y)
